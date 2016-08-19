8fd61998b1520f212fa5138bd16e46f7c1b7b5c9
DJ Sabo <sabodj@pdx.edu>
2016-08-18 20:39:15 -0700

    Refactored the way tests were added to make it cleaner Need to run with -Xint
    Bucket, Merge, Quicksort, Heap, and Java sort all working.

--- src/Sorting/Runner.java
+++ src/Sorting/Runner.java
@@ -1,6 +1,8 @@
 package Sorting;
 
 
+import java.util.ArrayList;
+
 /**
  * Main Class to run the sorting algorithms
  * Need to run with '-Xint'  to make sure we don't use optimizations
@@ -14,17 +16,26 @@ public class Runner {
      * The sizes[] and ranges[] arrays must have the same number of elements
      */
     public  void run(){
-//        int[] sizes = {1000000, 1000000, 5000000, 5000000, 10000000, 10000000};
-//        int[] ranges = {10, 1000000, 10, 5000000, 10, 10000000};
-        int[] sizes = {100000, 100000, 5000000, 5000000, 10000000, 10000000};
-        int[] ranges = {10000000, 100000, 1000000, 5000000, 10000000, 100000000};
-
-        if(sizes.length != ranges.length){
-            System.err.println("Sizes and Ranges must contain the same number of elements");
-            System.exit(1);
+        class TestCase {
+            public int size;
+            public int range;
+            TestCase(int size, int range){
+                this.size = size;
+                this.range = range;
+            }
         }
-        for(int i = 0; i < sizes.length; i++){
-            runTests(sizes[i], ranges[i]);
+        ArrayList<TestCase> tests = new ArrayList<TestCase>();
+        // You can add tests cases here the first number is the array size
+        // and the second is the number range
+        tests.add(new TestCase(1000000, 1000000));
+        tests.add(new TestCase(1000000, 5000000));
+        tests.add(new TestCase(5000000, 1000000));
+        tests.add(new TestCase(5000000, 5000000));
+        tests.add(new TestCase(10000000, 10000000));
+
+        // Runs all of the test cases
+        for(TestCase aCase : tests){
+            runTests(aCase.size, aCase.range);
         }
 
         System.out.println("Completed Sorting Tests, calculating medians");
@@ -42,35 +53,35 @@ public class Runner {
         ListBuilder mainList2 = new ListBuilder(size, range);
         ListBuilder mainList3 = new ListBuilder(size, range);
 
-        new Timer(mainList1).timeMe( "Random Run 1", printer);
+        new Timer(mainList1).timeMe( "Random Run  ", printer);
         new Timer(mainList1).timeMe( "Random Run 2", printer);
         new Timer(mainList1).timeMe( "Random Run 3", printer);
 
         mainList1.randomizePercentage(10);
         mainList2.randomizePercentage(10);
         mainList3.randomizePercentage(10);
-        new Timer(mainList1).timeMe( "10% Random 1", printer);
+        new Timer(mainList1).timeMe( "10% Random  ", printer);
         new Timer(mainList1).timeMe( "10% Random 2", printer);
         new Timer(mainList1).timeMe( "10% Random 3", printer);
 
         mainList1.randomizePercentage(50);
         mainList2.randomizePercentage(50);
         mainList3.randomizePercentage(50);
-        new Timer(mainList1).timeMe( "50% Random 1", printer);
+        new Timer(mainList1).timeMe( "50% Random  ", printer);
         new Timer(mainList1).timeMe( "50% Random 2", printer);
         new Timer(mainList1).timeMe( "50% Random 3", printer);
 
         mainList1.sortList();
         mainList2.sortList();
         mainList3.sortList();
-        new Timer(mainList1).timeMe( "Sorted 1    ", printer);
+        new Timer(mainList1).timeMe( "Sorted      ", printer);
         new Timer(mainList1).timeMe( "Sorted 2    ", printer);
         new Timer(mainList1).timeMe( "Sorted 3    ", printer);
 
         mainList1.reverse();
         mainList2.reverse();
         mainList3.reverse();
-        new Timer(mainList1).timeMe( "Reversed 1  ", printer);
+        new Timer(mainList1).timeMe( "Reversed    ", printer);
         new Timer(mainList1).timeMe( "Reversed 2  ", printer);
         new Timer(mainList1).timeMe( "Reversed 3  ", printer);
 
